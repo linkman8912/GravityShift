da29b23ad00f124f701b2f9c61141676c6d46631
linkman <linkman8912@proton.me>
2025-05-29 10:14:57 -0400

    disable walljumping on each surface before jumping on a different one or touching the ground

--- Assets/Scripts/movement/Wallrunning.cs
+++ Assets/Scripts/movement/Wallrunning.cs
@@ -10,12 +10,15 @@ public class Wallrunning : MonoBehaviour
   [SerializeField] private float wallRunForce = 200;
   [SerializeField] private float maxWallrunTime = 1.5f;
   [SerializeField] private float wallMomentumAngle = 40;
+  [SerializeField] private float wallrunDelay = 0.3f;
   //[SerializeField] private float walljumpDelayTime = 0.75f;
   private float wallrunTimer;
   //private float walljumpDelayTimer;
+  private bool readyToWallrun = true;
   [Header("Walljumping")]
   [SerializeField] private float walljumpUpForce = 100f;
   [SerializeField] private float walljumpSideForce = 50f;
+  private GameObject mostRecentWalljump;
 
   [Header("Input")]
   private float horizontalInput;
@@ -43,6 +46,7 @@ public class Wallrunning : MonoBehaviour
   // Update is called once per frame
   void Update() {
     StateMachine();
+    if (pm.grounded) mostRecentWalljump = null;
   }
 
   void FixedUpdate() {
@@ -65,7 +69,7 @@ public class Wallrunning : MonoBehaviour
     verticalInput = Input.GetAxisRaw("Vertical");
 
     // state 1: wallrunning
-    if ((wallLeft || wallRight) && verticalInput > 0 && AboveGround()) {
+    if ((wallLeft || wallRight) && verticalInput > 0 && AboveGround() && readyToWallrun) {
       if(!pm.wallrunning) {
         StartWallrun();
       }
@@ -116,11 +120,21 @@ public class Wallrunning : MonoBehaviour
   void Walljump() {
     //Debug.Log("Walljump");
     Vector3 wallNormal = wallRight ? rightWallHit.normal : leftWallHit.normal;
+    GameObject currentTarget = wallRight ? rightWallHit.collider.gameObject : leftWallHit.collider.gameObject;
+    if (currentTarget == mostRecentWalljump) return;
 
     Vector3 forceToApply = transform.up * walljumpUpForce / 10 + wallNormal * walljumpSideForce / 10;
     // reset force
     rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
     // add force
     rb.AddForce(forceToApply, ForceMode.Impulse);
+    mostRecentWalljump = currentTarget;
+    readyToWallrun = false;
+    Invoke("ResetWallrunDelay", wallrunDelay);
+  }
+
+  void ResetWallrunDelay() {
+    Debug.Log("ready to wallrun");
+    readyToWallrun = true;
   }
 }
